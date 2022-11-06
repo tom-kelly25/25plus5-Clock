4687560be31c24d23f9ead1bbe1077002430ab13
tom-kelly25 <107217874+tom-kelly25@users.noreply.github.com>
2022-11-06 14:50:52 +0000

    Session display

--- 25plus5-clock-app/src/App.js
+++ 25plus5-clock-app/src/App.js
@@ -1,9 +1,20 @@
-import "./App.css";
-
+import styles from "./App.css";
+import React from "react";
 function App() {
+  const [displayTime, setDisplayTime] = React.useState(25 * 60);
+  const formatTime = (time) => {
+    let minutes = Math.floor(time / 60);
+    let seconds = time % 60;
+
+    return (
+      (minutes < 10 ? "0" + minutes : minutes) +
+      ":" +
+      (seconds < 10 ? "0" + seconds : seconds)
+    );
+  };
   return (
     <div>
-      <h2>test</h2>
+      <h1>{formatTime(displayTime)}</h1>
     </div>
   );
 }
